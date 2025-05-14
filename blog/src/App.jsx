40085d9f7b23847bf453dbd1e4530b28db8e0fc1
IsaacK47 <isaackayombo41@gmail.com>
2025-05-14 17:10:07 +0200

    Adding comments

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -7,23 +7,29 @@ function App() {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
+        // Send a GET request to the placeholder API to retrieve posts
         const response = await fetch('https://jsonplaceholder.typicode.com/posts');
 
+        // Check if the response was successful
         if (!response.ok) {
           throw new Error(`Error: ${response.status} ${response.statusText}`);
         }
 
+         // Parse the JSON response into a JavaScript object
         const data = await response.json();
         setPosts(data);
       } catch (err) {
+        // If any error occurs store the error message
         setError(err.message);
       }
     };
 
+    // Calls the fetchPosts function 
     fetchPosts();
   }, []);
 
   return (
+    // Renders blog posts
     <div>
       <h1>Blog Posts</h1>
 

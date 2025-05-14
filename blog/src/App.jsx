3a2d8abc842a48b385921243d2626c92488bf78a
IsaacK47 <isaackayombo41@gmail.com>
2025-05-14 16:46:30 +0200

    Storing fetch in try and catch

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -4,7 +4,24 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState('');
 
-  
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        setPosts(data);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchPosts();
+  }, []);
 
   return ()
 }
